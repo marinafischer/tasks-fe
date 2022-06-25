c6dcfb3c86bd9fff64a8e6f71a6af5ab558ae533
marinafischer <marinamirandafischer@gmail.com>
2022-06-25 18:52:24 -0300

    feat: router implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Provider from './context/Provider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Tasks from './pages/Tasks';
 
 function App() {
   return (
     <Provider>
-      <div>
-        <h1>Iniciando</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={ <Login/> } />
+          <Route path="/tasks" element={ <Tasks/> } />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
