ab562c4ecd5b72242d53683add8055868865c624
marinafischer <marinamirandafischer@gmail.com>
2022-08-29 17:57:17 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-
+// teste
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
